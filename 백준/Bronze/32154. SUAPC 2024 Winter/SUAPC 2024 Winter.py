bd5e01f039b2781f010349b91f799dcc50460fe5
lji213 <96836516+lji213@users.noreply.github.com>
2024-10-14 11:43:57 +0900

    [Bronze V] Title: SUAPC 2024 Winter, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/32154. SUAPC 2024 Winter/SUAPC 2024 Winter.py
@@ -0,0 +1,14 @@
+team = {
+    1: '11\nA B C D E F G H J L M',
+    2: '9\nA C E F G H I L M',
+    3: '9\nA C E F G H I L M',
+    4: '9\nA B C E F G H L M',
+    5: '8\nA C E F G H L M',
+    6: '8\nA C E F G H L M',
+    7: '8\nA C E F G H L M',
+    8: '8\nA C E F G H L M',
+    9: '8\nA C E F G H L M',
+    10: '8\nA B C F G H L M'
+    }
+
+print(team[int(input())])
